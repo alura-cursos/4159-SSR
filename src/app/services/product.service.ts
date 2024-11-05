4d215414bf154e1b0c1648cbe6669eec0715a86c
nayannelbatista <nayannelopes.bd@gmail.com>
2024-11-05 16:09:41 -0300

    Video 1.5 - criando os métodos do serviço

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { createClient } from '@supabase/supabase-js';
 import { environment } from '../../environments/environment.development';
+import { from, Observable } from 'rxjs';
+import { Product } from '../interfaces/product';
 
 export const supabase = createClient(
   environment.supabaseUrl, 
@@ -12,5 +14,31 @@ export const supabase = createClient(
 })
 export class ProductService {
 
-  constructor() { }
+  getProducts(): Observable<Product[]> {
+    return from(
+      supabase
+        .from('products')
+        .select('*')
+        .then(({ data: products, error}) => {
+          if (error) throw new Error(error.message);
+          return products || []
+        })
+    )
+  }
+
+  getProductById(id: number): Observable<Product> {
+    return from(
+      supabase
+        .from('products')
+        .select('*')
+        .eq('id', id)
+        .single()
+        .then(({ data: product, error}) => {
+          if (error) throw new Error(error.message);
+          return product
+        })
+    )
+  }
+
+
 }
